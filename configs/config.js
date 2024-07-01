6977aa75afb0c0dcd640e6d9cfa0ceaa74381b91
Tomasz Gała <Kaxtom29@gmail.com>
2024-07-01 06:02:51 +0200

    change version

--- configs/config.js
+++ configs/config.js
@@ -4,7 +4,7 @@ module.exports = {
   token: process.env.TOKEN,
   prefix: "?",
   botAuthor: "<@465932200123301928>",
-  botVersion: "0.1v ALPHA",
+  botVersion: "0.2v ALPHA",
   description:
     "Nowy bot stworzony przez GalusGaming'a, który ma za zadanie ułatwić życie na serwerze.Bedzie posiadać różne funkcje od administracyjnych po te do zabawy.\n Bot jest w fazie ALPHA, więc mogą występować błędy. W razie problemów proszę o kontakt ze mną",
   DatabaseURL: process.env.MONGODB_URL,
