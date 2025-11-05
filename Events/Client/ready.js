b5a156b5ab3a289ad52cfab4898f2df60a78a71e
Galus <kaxtom29@gmail.com>
2025-11-05 11:03:26 +0100

    a little fixing errors!

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -1,5 +1,5 @@
 const { loadCommands } = require("../../handlers/commandHandler");
-const { Client, ActivityType, Collection } = require("discord.js");
+const { Client, PresenceUpdateStatus } = require("discord.js");
 const client = Client;
 const chalk = require("chalk");
 module.exports = {
@@ -10,6 +10,7 @@ module.exports = {
     loadCommands(client);
     // console.log(client.commands);
     // console.log(client.devCommands);
-    console.log(chalk.blue.bgRed.bold("Klient jest gotowy!"));
+    console.log(chalk.blue.bgRed.bold("Klient jest gotowy!"))
+    client.user.setPresence({ activities: [{ name: 'Wersja rozwojowa, nie zaleca sie używania bota' }], status: PresenceUpdateStatus.dnd });
   },
 };
